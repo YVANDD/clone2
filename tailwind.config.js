cf85f598f30eb1026a6cea2c6fa94bd27463abb6
nyvan80076@gmail.com <nyvan80076@gmail.com>
2023-06-17 01:58:00 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md:"768px",
+      lg:"976px",
+      xl:"1280px"
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
+
